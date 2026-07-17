208f02188e53351fb0803a5149f56693d67dacaf
NainaChugh1 <nchugh481@gmail.com>
2026-07-17 18:25:41 +0530

    Create Q16_Append_File.py

---
+++ Q16_Append_File.py
@@ -0,0 +1,9 @@
+# Q16 - Append a new line to student.txt
+
+file = open("student.txt", "a")
+
+file.write("\nPython file handling becomes simple with practice.")
+
+file.close()
+
+print("New line added successfully.")
