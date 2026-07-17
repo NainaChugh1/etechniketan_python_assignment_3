62637e28830838ef60b0950bdb37c330104fec59
NainaChugh1 <nchugh481@gmail.com>
2026-07-17 18:23:18 +0530

    Create Q11_Lambda_Add.py

---
+++ Q11_Lambda_Add.py
@@ -0,0 +1,8 @@
+# Q11 - Lambda function to add two numbers
+
+add = lambda a, b: a + b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Sum =", add(num1, num2))
